6511d1037726f19a53225d053933d19d14025add
nanodev21 <cristianorafael.com@gmail.com>
2022-08-24 22:42:24 +0000

    Arquivos IaC V1.0

---
+++ iac1.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários ADM...e adicionando no grupo ADM"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+
+echo "Criando usuário VEN...e adicionando no grupo VEN "
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+
+echo "Criando usuários SEC... e adicionando no grupo SEC"
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+
+
+echo "Especificando proprietário root e permissões dos diretórios"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Finalizado"
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
